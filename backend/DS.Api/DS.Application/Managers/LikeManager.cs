839cc1200350b78b75f0885b00bd2c32c803d58d
Mohit Vishwakarma <mohitvishwakarma681@gmail.com>
2025-12-03 00:05:08 +0530

    Code tweaks

--- backend/DS.Api/DS.Application/Managers/LikeManager.cs
+++ backend/DS.Api/DS.Application/Managers/LikeManager.cs
@@ -1,10 +1,12 @@
 ﻿namespace DS.Application.Managers
 {
-    public class LikeManager(ILikeRepository likeRpeository) : ILikeManager
+    public class LikeManager(ILikeRepository likeRpeository,IUnitOfWork unitOfWork) : ILikeManager
     {
         public async Task<bool> ToggleLike(int postId, int userId)
         {
-            return await likeRpeository.ToggleLike(postId, userId);
+            var res = await likeRpeository.ToggleLike(postId, userId);
+            await unitOfWork.SaveChangesAsync();
+            return res;
         }
     }
 }
